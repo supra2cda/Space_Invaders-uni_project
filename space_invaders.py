c944834a3949247abeaffd6a7f0437030b6cb94c
super2cda <andresoaresfonseca2007@gmail.com>
2025-11-17 17:54:24 +0000

    just missing files shii

--- space_invaders.py
+++ space_invaders.py
@@ -22,7 +22,7 @@ ENEMY_SIZE = 32
 ENEMY_START_Y = BORDA_Y - ENEMY_SIZE    # topo visível
 ENEMY_FALL_SPEED = 0.5
 ENEMY_DRIFT_STEP = 2
-ENEMY_FIRE_PROB = 0.005  # 0.006
+ENEMY_FIRE_PROB = 0.05  # 0.006
 ENEMY_BULLET_SPEED = 8
 ENEMY_INVERT_CHANCE = 0.05
 ENEMY_DRIFT_CHANCE = 0.5
@@ -212,7 +212,7 @@ def inimigos_disparam(state):  # DONE
                 xcor, ycor - ENEMY_BULLET_SPEED, "enemy_bullets"))
 
 
-def verificar_colisoes_player_bullets(state):  # falta acabar jogo
+def verificar_colisoes_player_bullets(state):  # DONE
     bullets = state["player_bullets"]
     enemies = state["enemies"]
 
@@ -223,6 +223,9 @@ def verificar_colisoes_player_bullets(state):  # falta acabar jogo
                     enemy.hideturtle()
                     enemies.remove(enemy)
 
+                    bullet.hideturtle()
+                    bullets.remove(bullet)
+
 
 def verificar_colisoes_enemy_bullets(state):  # DONE
     bullets = state["enemy_bullets"]
@@ -231,23 +234,25 @@ def verificar_colisoes_enemy_bullets(state):  # DONE
     for bullet in bullets:
         if (bullet.ycor() < (player.ycor() + COLLISION_RADIUS)) and (bullet.ycor() > (player.ycor() - COLLISION_RADIUS)):
             if (bullet.xcor() > (player.xcor() - COLLISION_RADIUS)) and (bullet.xcor() < (player.xcor() + COLLISION_RADIUS)):
-                player.hideturtle()
-                player.remove(player)
+                return True
 
 
-def inimigo_chegou_ao_fundo(state):
-    print("[inimigo_chegou_ao_fundo] por implementar")
+def inimigo_chegou_ao_fundo(state):  # DONE
+    enemies = state["enemies"]
+
+    for enemy in enemies:
+        if (enemy.ycor() == -BORDA_Y):
+            return True
 
 
-def verificar_colisao_player_com_inimigos(state): # falta acabar jogo
+def verificar_colisao_player_com_inimigos(state):  # DONE
     enemies = state["enemies"]
     player = state["player"]
 
     for enemy in enemies:
         if (enemy.ycor() < (player.ycor() + COLLISION_RADIUS)) and (enemy.ycor() > (player.ycor() - COLLISION_RADIUS)):
             if (enemy.xcor() > (player.xcor() - COLLISION_RADIUS)) and (enemy.xcor() < (player.xcor() + COLLISION_RADIUS)):
-                player.hideturtle()
-                player.remove(player)
+                return True
 
 
 # =========================
@@ -255,7 +260,8 @@ def verificar_colisao_player_com_inimigos(state): # falta acabar jogo
 # =========================
 if __name__ == "__main__":
     # Pergunta inicial: carregar?
-    filename = input("Carregar jogo? Se sim, escreva nome do ficheiro, senão carregue Return: ").strip()
+    filename = input(
+        "Carregar jogo? Se sim, escreva nome do ficheiro, senão carregue Return: ").strip()
     loaded = carregar_estado_txt(filename)
 
     # Ecrã
